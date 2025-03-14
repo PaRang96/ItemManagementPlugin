662bca27fdb294f42b9bbce4a1af0a8e8b0aac1c
Lee, Min Seok <winterreise94@gmail.com>
2025-03-14 20:17:21 +0900

    initial commit

---
+++ Source/ItemManagement/Private/ItemManagement.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ItemManagement.h"
+
+#define LOCTEXT_NAMESPACE "FItemManagementModule"
+
+void FItemManagementModule::StartupModule()
+{
+}
+
+void FItemManagementModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FItemManagementModule, ItemManagement)
---
+++ Source/ItemManagement/Public/ItemDefinitions.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ITEMMANAGEMENT_API ItemDefinitions
+{
+public:
+	ItemDefinitions();
+	~ItemDefinitions();
+};
